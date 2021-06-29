7b24bd87e817d9095facf8b2ea1de261836df6f2
Luciano Zancan <luciano.mazzutti@hypeflame.com.br>
2021-06-28 22:04:57 -0300

    *Steps até o 6

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -9,7 +9,7 @@ import timber.log.Timber
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
